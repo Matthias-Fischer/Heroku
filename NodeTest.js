0c1f0103636c9f9649dabcf83cc06e2d85f9e56f
matth <matth@LAPTOP-9O67TI3N>
2017-06-22 10:13:31 +0200

    Aufgabe 10

--- NodeTest.js
+++ NodeTest.js
@@ -1,3 +1,10 @@
+/*
+Aufgabe: 10 - Client Server
+Name: Matthias Fischer
+Datum: 22.06.2017
+Matrikel: 255035
+Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. Er wurde nicht kopiert und auch nicht diktiert.
+*/
 "use strict";
 console.log("Server starting");
 const Http = require("http");
@@ -15,13 +22,25 @@ function handleListen() {
 function handleRequest(_request, _response) {
     console.log("Request received");
     console.log(_request.url);
+    let query = Url.parse(_request.url, true).query;
     _response.setHeader("Access-Control-Allow-Origin", "*");
     _response.setHeader("content-type", "text/html; charset=utf-8");
-    let query = Url.parse(_request.url, true).query;
-    console.log(query);
     let key;
     for (key in query)
-        _response.write(key + ":" + query[key]);
-    _response.end();
+        console.log(key + ":" + query[key]);
 }
+//Heroku-Ausgabe
+_response.write("Du hast folgende Eissorten gew�hlt: <br>");
+_response.write("Vanille: " + query["Vanille"] + "<br>");
+_response.write("Schokolade: " + query["Schokolade"] + "<br>");
+_response.write("Erdbeere: " + query["Erdbeere"] + "<br>");
+_response.write("Haselnuss: " + query["Haselnuss"] + "<br>");
+_response.write("Joghurt: " + query["Joghurt"] + "<br>");
+_response.write("Banane: " + query["Banane"] + "<br>");
+_response.write("Zitrone: " + query["Zitrone"] + "<br>");
+_response.write("<br>");
+_response.write("Darreichungsform ist: " + query["Container"] + "<br>");
+_response.write("Toppings: " + query["Container"] + "<br>");
+_response.write("Die Bestellung wird an " + query["Adresse"] + " gesendet <br>");
+_response.end();
 //# sourceMappingURL=NodeTest.js.map
--- NodeTest.ts
+++ NodeTest.ts
@@ -1,3 +1,11 @@
+/*
+Aufgabe: 10 - Client Server 
+Name: Matthias Fischer
+Datum: 22.06.2017
+Matrikel: 255035
+Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. Er wurde nicht kopiert und auch nicht diktiert.
+*/
+
 console.log("Server starting");
 
 import Http = require("http");
@@ -24,16 +32,27 @@ function handleRequest(_request: Http.IncomingMessage, _response: Http.ServerRes
     console.log("Request received");
 
     console.log(_request.url);
-
+    let query: AssocStringString = Url.parse(_request.url, true).query;
     _response.setHeader("Access-Control-Allow-Origin", "*");
     _response.setHeader("content-type", "text/html; charset=utf-8");
-    let query: AssocStringString = Url.parse(_request.url, true).query;
-    console.log(query);
     let key: string;
     for (key in query)
-        _response.write(key + ":" + query[key]);
+        console.log(key + ":" + query[key]);
+}
 
+//Heroku-Ausgabe
 
-
-    _response.end();
+_response.write("Du hast folgende Eissorten gewählt: <br>");
+_response.write("Vanille: " + query["Vanille"] + "<br>");
+_response.write("Schokolade: " + query["Schokolade"] + "<br>");
+_response.write("Erdbeere: " + query["Erdbeere"] + "<br>");
+_response.write("Haselnuss: " + query["Haselnuss"] + "<br>");
+_response.write("Joghurt: " + query["Joghurt"] + "<br>");
+_response.write("Banane: " + query["Banane"] + "<br>");
+_response.write("Zitrone: " + query["Zitrone"] + "<br>");
+_response.write("<br>");
+_response.write("Darreichungsform ist: " + query["Container"] + "<br>");
+_response.write("Toppings: " + query["Container"] + "<br>");
+_response.write("Die Bestellung wird an " + query["Adresse"] + " gesendet <br>");
+_response.end();
 }
