7a57f4935c905fb119f483f95525d6814261af85
matth <matth@LAPTOP-9O67TI3N.hfu-open.fu.hs-furtwangen.de>
2017-06-22 13:03:29 +0200

    push

--- NodeTest.ts
+++ NodeTest.ts
@@ -35,24 +35,22 @@ function handleRequest(_request: Http.IncomingMessage, _response: Http.ServerRes
     let query: AssocStringString = Url.parse(_request.url, true).query;
     _response.setHeader("Access-Control-Allow-Origin", "*");
     _response.setHeader("content-type", "text/html; charset=utf-8");
-    let key: string;
-    for (key in query)
-        console.log(key + ":" + query[key]);
-}
 
-//Heroku-Ausgabe
 
-_response.write("Du hast folgende Eissorten gewählt: <br>");
-_response.write("Vanille: " + query["Vanille"] + "<br>");
-_response.write("Schokolade: " + query["Schokolade"] + "<br>");
-_response.write("Erdbeere: " + query["Erdbeere"] + "<br>");
-_response.write("Haselnuss: " + query["Haselnuss"] + "<br>");
-_response.write("Joghurt: " + query["Joghurt"] + "<br>");
-_response.write("Banane: " + query["Banane"] + "<br>");
-_response.write("Zitrone: " + query["Zitrone"] + "<br>");
-_response.write("<br>");
-_response.write("Darreichungsform ist: " + query["Container"] + "<br>");
-_response.write("Toppings: " + query["Container"] + "<br>");
-_response.write("Die Bestellung wird an " + query["Adresse"] + " gesendet <br>");
-_response.end();
+    //Heroku-Ausgabe
+
+    _response.write("Hallo " + query["Vorname"] + " danke fuer deine Bestellung" + "<br>" + "Deine Eisauswahl:" + "<br>");
+    _response.write("Vanille: " + query["Vanille"] + "<br>");
+    _response.write("Schokolade: " + query["Schokolade"] + "<br>");
+    _response.write("Erdbeere: " + query["Erdbeere"] + "<br>");
+    _response.write("Haselnuss: " + query["Haselnuss"] + "<br>");
+    _response.write("Joghurt: " + query["Joghurt"] + "<br>");
+    _response.write("Banane: " + query["Banane"] + "<br>");
+    _response.write("Zitrone: " + query["Zitrone"] + "<br>");
+    _response.write("<br>");
+    _response.write("Darreichungsform ist: " + query["darbietung"] + "<br>");
+    _response.write("Toppings: " + query["topping"] + "<br>");
+    _response.write("Lieferoption: " + query["delivery"] + "<br>");
+    _response.write("Die Bestellung wird an " + query["Strasse"] + query["Ort"] + " gesendet <br>");
+    _response.end();
 }
